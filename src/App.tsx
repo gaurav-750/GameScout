0162ea87abdf5d28a5dbe24dead000ded0d44dbf
Gaurav <gauravsomani52750@gmail.com>
2023-03-22 20:28:22 +0530

    layout using grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Show } from "@chakra-ui/react";
 import "./App.css";
+import { Grid, GridItem } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Button colorScheme="teal" size="lg">
-        Button
-      </Button>
-    </div>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem pl="2" bg="white" area={"nav"}>
+          <Navbar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem pl="2" bg="coral" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem pl="2" bg="yellow.200" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const Navbar = () => {
+  return (
+    <>
+      <HStack>
+        <Image src={logo} boxSize="50px" objectFit="cover" />
+        <Text>NavBar</Text>
+      </HStack>
+    </>
+  );
+};
+
+export default Navbar;
